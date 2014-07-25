0dc738fd9bc01e39c45908c3192fedfcc7abc737
Tommy-Zhang <liang.z@buaa.edu.cn>
2014-07-25 21:01:19 +0800

    new

---
+++ YetAnotherSample/NewClass.h
@@ -0,0 +1,13 @@
+//
+//  NewClass.h
+//  YetAnotherSample
+//
+//  Created by ZhangLiang on 7/25/14.
+//  Copyright (c) 2014 ZhangLiang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NewClass : NSObject
+
+@end
---
+++ YetAnotherSample/NewClass.m
@@ -0,0 +1,13 @@
+//
+//  NewClass.m
+//  YetAnotherSample
+//
+//  Created by ZhangLiang on 7/25/14.
+//  Copyright (c) 2014 ZhangLiang. All rights reserved.
+//
+
+#import "NewClass.h"
+
+@implementation NewClass
+
+@end
